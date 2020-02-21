1c16684bb51bd7f97e2bd0f16bfc6759f55f78c4
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 04:42:59 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  collection = []
-  while i < array.length
+  while i < collection.length
+    yield(collection[i])
     i += 1
-    yield array[0]
-    
+  end
 end
+
